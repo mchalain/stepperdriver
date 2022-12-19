537ced32e8f8ce76183819a87d7d6ca9820e41a3
mchalain <marc.chalain@scantech.com>
2022-12-19 14:33:00 +0100

    [driver] re-indent

--- stepperdriver.cpp
+++ stepperdriver.cpp
@@ -14,12 +14,12 @@ Stepper::Stepper(int en, int step, int dir, unsigned int max, int end, int enSta
     _position(0), _speed(0), _nbsteps(0), _accel(10), _minspeed(100), _maxspeed(2000),
     _stepsmm(1)
 {
-  pinMode(this->enPin,OUTPUT); 
-  pinMode(this->stepPin,OUTPUT); 
-  pinMode(this->dirPin,OUTPUT);
-  if (this->endPin != -1)
-    pinMode(this->endPin,INPUT);
-  digitalWrite(this->enPin, !this->_enable);
+	pinMode(this->enPin,OUTPUT);
+	pinMode(this->stepPin,OUTPUT);
+	pinMode(this->dirPin,OUTPUT);
+	if (this->endPin != -1)
+		pinMode(this->endPin,INPUT);
+	digitalWrite(this->enPin, !this->_enable);
 }
 void Stepper::setup(Stepper::Setting setting, int value)
 {
@@ -47,120 +47,120 @@ void Stepper::setup(Stepper::Setting setting, int value)
 }
 int Stepper::turn(int nbsteps, int speed)
 {
-  if (enabled())
-    return -1;
-  int dir = LOW;
-  if (nbsteps<0)
-  {
-    this->_state |= NEGATIVSENS;
-    dir = HIGH;
-    this->_nbsteps = -nbsteps;
-  }
-  else
-  {
-    this->_state &= ~NEGATIVSENS;
-    this->_nbsteps = nbsteps;
-  }
-  this->_speed = this->_minspeed;
-  if (this->_state & MILLIMODE)
-    speed /= this->_stepsmm;
-  this->_speedtarget = (speed < this->_maxspeed)?speed:this->_maxspeed;
-  digitalWrite(this->dirPin, dir);
-  return 0;
+	if (enabled())
+	return -1;
+	int dir = LOW;
+	if (nbsteps<0)
+	{
+		this->_state |= NEGATIVSENS;
+		dir = HIGH;
+		this->_nbsteps = -nbsteps;
+	}
+	else
+	{
+		this->_state &= ~NEGATIVSENS;
+		this->_nbsteps = nbsteps;
+	}
+	this->_speed = this->_minspeed;
+	if (this->_state & MILLIMODE)
+		speed /= this->_stepsmm;
+	this->_speedtarget = (speed < this->_maxspeed)?speed:this->_maxspeed;
+	digitalWrite(this->dirPin, dir);
+	return 0;
 }
 void Stepper::home(int speed)
 {
-  if (this->endPin != -1)
-    this->_position = 1;
-  turn(-1 * this->_position, speed);
-  if (this->endPin != -1)
-    this->_state |= HOMING;
+	if (this->endPin != -1)
+		this->_position = 1;
+	turn(-1 * this->_position, speed);
+	if (this->endPin != -1)
+		this->_state |= HOMING;
 }
 void Stepper::start()
 {
-  if ((this->_nbsteps == 0) ||
-    (this->_position == 0 && (this->_state & NEGATIVSENS)) ||
-    (this->_position == this->_max && !(this->_state & NEGATIVSENS)))
-  {
-    return;
-  }
-  digitalWrite(this->enPin, this->_enable);
-  this->_ptime = this->_time() + ((MAXSTEPS / 2) / this->_speed);
+	if ((this->_nbsteps == 0) ||
+		(this->_position == 0 && (this->_state & NEGATIVSENS)) ||
+		(this->_position == this->_max && !(this->_state & NEGATIVSENS)))
+	{
+		return;
+	}
+	digitalWrite(this->enPin, this->_enable);
+	this->_ptime = this->_time() + ((MAXSTEPS / 2) / this->_speed);
 }
 int Stepper::_time()
 {
-  return micros();
+	return micros();
 }
 int Stepper::_checktimer()
 {
-  int ret = (this->_ptime - this->_time()) <= 0;
-  if (ret)
-    this->_ptime += ((MAXSTEPS / 2) / this->_speed);
-  if (this->_speed < this->_speedtarget)
-	  this->_speed += this->_accel;
-  return ret;
+	int ret = (this->_ptime - this->_time()) <= 0;
+	if (ret)
+		this->_ptime += ((MAXSTEPS / 2) / this->_speed);
+	if (this->_speed < this->_speedtarget)
+		this->_speed += this->_accel;
+	return ret;
 }
 void Stepper::stop()
 {
-  digitalWrite(this->enPin, !this->_enable);
-  this->_nbsteps = 0;
+	digitalWrite(this->enPin, !this->_enable);
+	this->_nbsteps = 0;
 }
 int Stepper::enabled()
 {
-  return (digitalRead(this->enPin) == this->_enable);
+	return (digitalRead(this->enPin) == this->_enable);
 }
 unsigned int Stepper::position()
 {
-  return this->_position;
+	return this->_position;
 }
 void Stepper::_handler()
 {
-  if (this->_state & STEPHIGH)
-  {
-    digitalWrite(this->stepPin,HIGH);
-    this->_state &= ~STEPHIGH;
-  }
-  else
-  {
-    digitalWrite(this->stepPin,LOW);
-    this->_state |= STEPHIGH;
-    this->_position += (this->_state & NEGATIVSENS)? -1 : 1;
-    this->_nbsteps--;
-  }
+	if (this->_state & STEPHIGH)
+	{
+		digitalWrite(this->stepPin,HIGH);
+		this->_state &= ~STEPHIGH;
+	}
+	else
+	{
+		digitalWrite(this->stepPin,LOW);
+		this->_state |= STEPHIGH;
+		this->_position += (this->_state & NEGATIVSENS)? -1 : 1;
+		this->_nbsteps--;
+	}
 }
 int Stepper::step(int speed)
 {
-  if (!enabled())
-    return  0;
-  if (this->endPin != -1 && (this->_state & NEGATIVSENS))
-  {
-    if (this->_state & HOMING)
-    {
-      this->_position = 1; // keep the position positiv while end sensor is LOW
-      this->_nbsteps = 1;
-    }
-    if (digitalRead(this->endPin) == LOW)
-    {
-      stop();
-      this->_position = 0;
-      this->_state &= ~HOMING;
-      return -1;
-    }
-  }
-  if ((this->_nbsteps == 0) ||
-    (this->_position == 0 && (this->_state & NEGATIVSENS)) ||
-    (this->_position == this->_max && !(this->_state & NEGATIVSENS)))
-  {
-    stop();
-    return 0;
-  }
-  if (_checktimer())
-  {
-    _handler();
-  }
-  return this->_nbsteps;
+	if (!enabled())
+		return  0;
+	if (this->endPin != -1 && (this->_state & NEGATIVSENS))
+	{
+		if (this->_state & HOMING)
+		{
+			this->_position = 1; // keep the position positiv while end sensor is LOW
+			this->_nbsteps = 1;
+		}
+		if (digitalRead(this->endPin) == LOW)
+		{
+			stop();
+			this->_position = 0;
+			this->_state &= ~HOMING;
+			return -1;
+		}
+	}
+	if ((this->_nbsteps == 0) ||
+		(this->_position == 0 && (this->_state & NEGATIVSENS)) ||
+		(this->_position == this->_max && !(this->_state & NEGATIVSENS)))
+	{
+		stop();
+		return 0;
+	}
+	if (_checktimer())
+	{
+		_handler();
+	}
+	return this->_nbsteps;
 }
 int Stepper::step()
 {
-  return step(this->_speed);
+	return step(this->_speed);
 }
